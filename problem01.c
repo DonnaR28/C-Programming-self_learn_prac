3994926bdefc40e0ef86ce595c2de6ebed683e43
Donna R <117378972+DonnaR28@users.noreply.github.com>
2023-03-16 06:51:04 +0000

    .

---
+++ problem01.c
@@ -0,0 +1,9 @@
+//Program to print ASCII Value .
+#include<stdio.h>
+int main(){
+  char c;
+  printf("Enter a character: ");
+  if(scanf("%c", &c));
+  printf("The ASCII value of %c is %d", c, c);
+  return 0;
+}
